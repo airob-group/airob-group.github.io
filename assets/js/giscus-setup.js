3bc2b556eded8926b337df74db577bf05cdd3d2b
AIRob-group <hangma@sfu.ca>
2025-11-13 22:57:19 +0000

    Deploying to gh-pages from @ AIRob-group/airob-group.github.io@20d2d7840f5383da040154467888918c5d1a77b1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "airob-group/airob-group.github.io",
+    "data-repo": "AIRob-group/airob-group.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
